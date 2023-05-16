9ff691105be3992b4738a6748d14a9734340ece9
jerry <jerrynwosu007@gmail.com>
2023-05-16 22:07:42 +0100

    update

--- src/components/LatestMovie.tsx
+++ src/components/LatestMovie.tsx
@@ -1,14 +1,10 @@
+// @ts-nocheck
+
 import { useQuery } from '@tanstack/react-query';
 import { getLatestMovie } from '../api/movies';
 import { Title } from '../styles/gobalStyles';
 import Movie from './Movie';
 import { IMovie } from '../types/movie';
-
-interface MovieII {
-  id: number;
-  backdrops: [];
-  posters: [];
-}
 
 const LatestMovie = () => {
   const { data: movie } = useQuery<void, unknown, IMovie>({
